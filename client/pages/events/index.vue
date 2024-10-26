9e7079c7bc222f804a6a4c8b2b05f88b22400c2b
salhe123 <salheseid92@gmail.com>
2024-10-26 14:34:41 +0300

    loin and front end setu the fron end

--- client/pages/events/index.vue
+++ client/pages/events/index.vue
@@ -1,8 +1,11 @@
 <!-- Events listing page with filters -->
+<script>
+import EventForm from "~/components/EventForm.vue";
+// import EventForm from '~/components/EventForm.vue';
+</script>
 
 <template>
-
+  <div>
+    <EventForm />
+  </div>
 </template>
-<script>
-
-</script>
